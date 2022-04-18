391ab56e1444bf8a05c6c13f148df35b32d09bb0
J S <jskrzynski@student.agh.edu.pl>
2022-04-18 22:28:00 +0200

    Initial commit, spring config files v2.6.6 for java 11 Build tool: Maven -Spring Web -Thymeleaf -Spring Security -PostgreSQL Driver -Java Mail Sender
    Signed-off-by: J S <jskrzynski@student.agh.edu.pl>

---
+++ src/main/java/project/softwarestudio/angel/AngelApplication.java
@@ -0,0 +1,13 @@
+package project.softwarestudio.angel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngelApplication.class, args);
+	}
+
+}
