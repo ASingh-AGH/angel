674ea9187900a6fca882898e54d4e19cf68c32f9
J S <jskrzynski@student.agh.edu.pl>
2022-04-19 18:57:44 +0200

    Getters and setters for Location class
    Signed-off-by: J S <jskrzynski@student.agh.edu.pl>

--- src/main/java/project/softwarestudio/angel/Location.java
+++ src/main/java/project/softwarestudio/angel/Location.java
@@ -34,4 +34,60 @@ public class Location {
      * Zip code
      */
     private String zip;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Point getGeo() {
+        return geo;
+    }
+
+    public void setGeo(Point geo) {
+        this.geo = geo;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public String getApartment_number() {
+        return apartment_number;
+    }
+
+    public void setApartment_number(String apartment_number) {
+        this.apartment_number = apartment_number;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
 }
