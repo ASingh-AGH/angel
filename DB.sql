c6e5d94fe8a211b0690dfb3231786dffa72a8b6e
J S <jskrzynski@student.agh.edu.pl>
2022-05-12 19:03:43 +0200

    New table in sql - angel.Locations
    Signed-off-by: J S <jskrzynski@student.agh.edu.pl>

--- DB.sql
+++ DB.sql
@@ -1,5 +1,12 @@
+-- SCHEMA: angel
+
+-- DROP SCHEMA angel ;
+
+CREATE SCHEMA angel
+    AUTHORIZATION postgres;
+
 -- This table should store all information about the location address to refer it in other tables
-  CREATE TABLE Location(
+CREATE TABLE angel.Location(
   	id SERIAL PRIMARY KEY,
   	geo GEOGRAPHY,
   	building TEXT,
@@ -9,13 +16,24 @@
   	zip TEXT
   );
 
-  CREATE TABLE HelpPlace(
+CREATE TABLE angel.HelpPlace(
   	id SERIAL PRIMARY KEY,
   	name TEXT,
   	description TEXT,
   	locationId INT,
   	accepted BOOL,
-  	CONSTRAINTS location_id_fk
-  		FOREIGN KEY (locationId)
-  		REFERENCES Location(id)
+  	CONSTRAINT location_id_fk
+  	FOREIGN KEY (locationId)
+  	REFERENCES angel.Location(id)
   );
+
+
+CREATE TABLE angel.Locations(
+    id SERIAL PRIMARY KEY,
+    geo GEOGRAPHY,
+    building TEXT,
+    apartmentNumber TEXT,
+    street TEXT,
+    city TEXT,
+    zip TEXT
+);
